00d3beedcc375574523d6f142ec738768c8b4ea3
Виктор <vitya.bulgakov@ukr.net>
2023-07-09 17:10:57 +0300

    Classes "Burger" and "BurgerMain"

--- HW_3/BurgerMain.java
+++ HW_3/BurgerMain.java
@@ -2,7 +2,7 @@ package HW_3;
 
 public class BurgerMain {
     public static void main(String[] args) {
-        Burger regularBurger = new Burger("Bun", "Beef", "Cheese", "Salad", "Mayonnaise");
+        Burger normalBurger = new Burger("Bun", "Beef", "Cheese", "Salad", "Mayonnaise");
 
         Burger dietBurger = new Burger("Bun", "Chicken", "Cheese", "Salad");
 
