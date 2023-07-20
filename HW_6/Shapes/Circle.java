1f121db9f4ce4ac55b3779eddcd14d061365770a
Виктор <vitya.bulgakov@ukr.net>
2023-07-20 18:34:48 +0300

    Shapes

---
+++ HW_6/Shapes/Circle.java
@@ -0,0 +1,14 @@
+package HW_6.Shapes;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ HW_6/Shapes/Square.java
@@ -0,0 +1,14 @@
+package HW_6.Shapes;
+
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+}
